579cfdf95a612cc9f426d21fbcf71734de1a5c04
thak1411 <thak1411@gmail.com>
2020-01-12 14:38:55 +0900

    init project

---
+++ src/pages/dashboard/App.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+div#app
+    dashboard-root
+</template>
+
+<script>
+import DashboardRoot from "./Root.vue";
+import mixins from "../mixins/mixins.default.js";
+export default {
+    name: 'app',
+    mixins: [mixins],
+    components: {
+        DashboardRoot
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+body, html {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+}
+#app{
+    width:100%;
+    height: 100%;
+}
+</style>
---
+++ src/views/global/global-abs-components.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+div.global-abs-components
+    //- tooltip-abs
+</template>
+
+<script>
+export default {
+    name: 'global-abs-components',
+    components: {
+        // TooltipAbs
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
